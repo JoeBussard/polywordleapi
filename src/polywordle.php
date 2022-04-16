6ac03681722a9999b113550b36ff6d49ca57edb1
joe bussard <jbussard@ucsc.edu>
2022-04-16 16:37:57 -0700

    Include PHP testing page

--- src/polywordle.php
+++ src/polywordle.php
@@ -1,12 +1,33 @@
 <?php 
 
 require 'sage.phar';
-//sage('hello world');
+sage($_POST);
+sage($_GET);
+sage('hello world');
+  if (isset($_POST['submitStartNewGame'])) {
+    $new_uuid = curl_new_game();  
+sage($new_uuid);
+    $_SESSION['uuid'] = $new_uuid['game_uuid'];
+    $game_data = curl_game($new_uuid['game_uuid']);
+  }
+  else if (isset($_POST['submitNewGuess'])){
+    $_SESSION['uuid'] = $_POST['old_uuid'];
+    curl_new_guess($_POST['newGuess'], $_SESSION['uuid']); 
+    $game_data = curl_game($_SESSION['uuid']);
+  }
+    
 
-  function curl_game ($userId = "") {
+  function curl_game ($userId) {
     $ch = curl_init();
-    $base_url = '127.0.0.1:5000/game';
-    $game_url = $base_url . $userId;
+    $base_url = '127.0.0.1:5000/v1/game';
+// do not continue if uuid is not set.
+/*    if ($userId = "") {
+      $game_url = $base_url;
+    }
+    else { 
+      $game_url = $base_url . '/' . $userId;
+    }*/
+    $game_url = $base_url . '/' . $_SESSION['uuid'];
     curl_setopt($ch, CURLOPT_URL, $game_url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     $result_raw = curl_exec($ch);
@@ -14,16 +35,16 @@ require 'sage.phar';
     return $result_json;
     curl_close($ch);
   }
-
+/***********88
   if (isset($_POST['user_id'])){
     $result = curl_game($_POST['user_id']);
-    $user_id = $_POST['user_id'];
+    $user_id = $_POST['uuid'];
   }
   else{
     $result = curl_game();
-    $user_id = $result['user_id'];
+    $user_id = $result['uuid'];
   }
-
+********/
 /*
   $ch = curl_init();
   $base_url = '127.0.0.1:5000/game/curl_1';
@@ -35,6 +56,8 @@ require 'sage.phar';
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
   $result = curl_exec($ch);
   curl_close($ch);*/
+sage($_SESSION['uuid']);
+
 ?>
 <html>
 <head>
@@ -49,13 +72,23 @@ require 'sage.phar';
 //  echo '<pre>' . var_export($result, true) . '</pre>' ;
 
 //  $obj = json_decode($result, true);
-  $obj = $result;
+  sage($obj);
+  //$obj = $result;
 //  sage($obj);
 //  sage($obj_false);
   //echo("----@@@@@@@@@@@@------------------");
   echo("Polywordle for: ");
-  if (null != $obj) {
-  echo $obj['user_id'];
+sage($game_data);
+  if (null != $game_data) {
+    echo nl2br($game_data['user_id'] . "\n");
+	if (isset($obj)){
+		sage($obj);
+}
+?>
+<strong>Game UUID</strong>
+<h3><?php echo $game_data['uuid']?></h3>
+
+<?php
   $keyboard_map = $obj['keyboard_map'];
   $keys = array_keys($keyboard_map);
 //  sage($keys);
@@ -93,20 +126,27 @@ require 'sage.phar';
 
   function curl_new_guess ($guess, $user_id){
     $ch = curl_init();
-    $base_url = '127.0.0.1:5000/game/' . $user_id;
-    $guess_url = $base_url . '/' . $guess;
-    curl_setopt($ch, CURLOPT_URL, $guess_url);
+    $base_url = '127.0.0.1:5000/v1/game/' . $user_id;
+    curl_setopt($ch, CURLOPT_URL, $base_url);
+    $guess_data = [
+      'guess'=>$guess,];
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $guess_data);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    sage(curl_getinfo($ch));
     $result_raw = curl_exec($ch);
     $result_json = json_decode($result_raw, true);
     return $result_json;
   }
-  if (isset($_POST['submitNewGuess']) && isset($_POST['newGuess'])){
-    $result = curl_new_guess($_POST['newGuess'], $user_id);
+  /**if (isset($_POST['submitNewGuess']) && isset($_POST['newGuess'])){
+    $result = curl_new_guess($_POST['newGuess'], $game_data['uuid']);
   }
+  if (isset($_POST['submitStartNewGame'])) {
+    $obj = curl_new_game();  
+  }**/
   function curl_new_game () {
     $ch = curl_init();
-    $base_url = '127.0.0.1:5000/game';
+    $base_url = '127.0.0.1:5000/v1/game';
     curl_setopt($ch, CURLOPT_URL, $base_url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     $result_raw = curl_exec($ch);
@@ -122,6 +162,7 @@ require 'sage.phar';
 <form action = "" method='post'>
 <p><strong>My Next 5 Letter Word is...</strong></p>
 <input type='text' name='newGuess' maxlength='5'/>
+<input type='text' name='old_uuid' value="<?php echo $game_data['uuid']?>"/>
 <input type='submit' name='submitNewGuess'/>
 </form>
 <h3>Start A New Game?</h3>
